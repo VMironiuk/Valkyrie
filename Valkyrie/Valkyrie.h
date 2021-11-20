746f13c89b12349d398e6160c033f866a546b19c
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-11-20 12:24:29 +0200

    Initial Commit

---
+++ Valkyrie/Valkyrie.h
@@ -0,0 +1,18 @@
+//
+//  Valkyrie.h
+//  Valkyrie
+//
+//  Created by Vladimir Mironiuk on 20.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Valkyrie.
+FOUNDATION_EXPORT double ValkyrieVersionNumber;
+
+//! Project version string for Valkyrie.
+FOUNDATION_EXPORT const unsigned char ValkyrieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Valkyrie/PublicHeader.h>
+
+
