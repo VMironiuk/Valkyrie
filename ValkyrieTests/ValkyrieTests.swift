4f53fa953cbb0b915a94011012b22d1a4a8dba64
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-11-20 13:37:21 +0200

    Assert that the greeting property returns the greeting message

--- ValkyrieTests/ValkyrieTests.swift
+++ ValkyrieTests/ValkyrieTests.swift
@@ -12,6 +12,7 @@ private final class Valkyrie {
     // MARK: - Properties
 
     let name = "Valkyrie"
+    let greeting = "Hei Verden!"
 }
 
 class ValkyrieTests: XCTestCase {
@@ -19,5 +20,10 @@ class ValkyrieTests: XCTestCase {
     func test_name_returnsName() {
         let sut = Valkyrie()
         XCTAssertEqual(sut.name, "Valkyrie")
+    }
+
+    func test_greeting_returnsGreetingMessage() {
+        let sut = Valkyrie()
+        XCTAssertEqual(sut.greeting, "Hei Verden!")
     }
 }
