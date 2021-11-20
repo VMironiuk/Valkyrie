44c8bc6edab3e60283ae5d1829bf6b2b73defd37
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-11-20 12:57:10 +0200

    Assert that the name property returns robot's name

--- ValkyrieTests/ValkyrieTests.swift
+++ ValkyrieTests/ValkyrieTests.swift
@@ -7,5 +7,17 @@
 
 import XCTest
 
+private final class Valkyrie {
+
+    // MARK: - Properties
+
+    let name = "Valkyrie"
+}
+
 class ValkyrieTests: XCTestCase {
+
+    func test_name_returnsName() {
+        let sut = Valkyrie()
+        XCTAssertEqual(sut.name, "Valkyrie")
+    }
 }
