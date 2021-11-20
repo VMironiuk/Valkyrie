b1a60d6433a243969e8a2cdde54082d456e6a78f
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-11-20 17:10:35 +0200

    Assert that the robot can shoot to the center of the 3x3 tile

--- ValkyrieTests/ValkyrieTests.swift
+++ ValkyrieTests/ValkyrieTests.swift
@@ -9,17 +9,35 @@ import XCTest
 
 private final class Valkyrie {
 
-    // MARK: - Properties
+    // MARK: - Public Properties
 
     let name = "Valkyrie"
     let greeting = "Hei Verden!"
     let winMessage = "Slik blir det med alle!"
     let loseMessage = "Faen!🤯"
 
+    // MARK: - Private Properties
+
+    let tile: CGRect
+
+    // MARK: - Initializers
+
+    init(tile: CGRect) {
+        self.tile = tile
+    }
+
     // MARK: - Public Methods
 
     func shoot() -> CGPoint {
-        CGPoint(x: 0, y: 0)
+        if tile == CGRect.zero {
+            return CGPoint.zero
+        }
+
+        if Int(tile.width) % 2 == 0 {
+            return CGPoint.zero
+        } else {
+            return CGPoint(x: Int(tile.width) / 2, y: Int(tile.height) / 2)
+        }
     }
 }
 
@@ -28,31 +46,44 @@ class ValkyrieTests: XCTestCase {
     // MARK: - Tests for basic properties
 
     func test_name_returnsName() {
-        let sut = Valkyrie()
+        let sut = makeSUT()
         XCTAssertEqual(sut.name, "Valkyrie")
     }
 
     func test_greeting_returnsGreetingMessage() {
-        let sut = Valkyrie()
+        let sut = makeSUT()
         XCTAssertEqual(sut.greeting, "Hei Verden!")
     }
 
     func test_winMessage_returnsWinMessage() {
-        let sut = Valkyrie()
+        let sut = makeSUT()
         XCTAssertEqual(sut.winMessage, "Slik blir det med alle!")
     }
 
     func test_loseMessage_returnsLoseMessage() {
-        let sut = Valkyrie()
+        let sut = makeSUT()
         XCTAssertEqual(sut.loseMessage, "Faen!🤯")
     }
 
     // MARK: - Tests for simple shooting
 
     func test_shoot_returnsSomePointForUnspecifedBoard() {
-        let sut = Valkyrie()
+        let sut = makeSUT()
         let expectedShootPoint = CGPoint(x: 0, y: 0)
         let actualShootPoint = sut.shoot()
         XCTAssertEqual(expectedShootPoint, actualShootPoint)
+    }
+
+    func test_shoot_returnsCenterPointFor3x3Tile() {
+        let sut = makeSUT(tile: CGRect(x: 0, y: 0, width: 3, height: 3))
+        let expectedShootPoint = CGPoint(x: 1, y: 1)
+        let actualShootPoint = sut.shoot()
+        XCTAssertEqual(expectedShootPoint, actualShootPoint)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(tile: CGRect = .zero) -> Valkyrie {
+        Valkyrie(tile: tile)
     }
 }
